64706446dd2a86b6b7228e03670c78b46b819e26
jcoehoorn <jcoehoorn@gmail.com>
2022-06-14 14:50:32 -0500

    Use expression-bodied members

--- main.cs
+++ main.cs
@@ -46,54 +46,52 @@ public static class SSUtil
     public static void P(O s) { System.Diagnostics.Trace.WriteLine(s); }
     public static void p(O s) {System.Diagnostics.Trace.Write(s); }
     
-    public static string R() { return Console.ReadLine(); }
-    public static string R(S prompt) { p(prompt); return R(); }
+    public static S R() => Console.ReadLine();
+    public static S R(S prompt) { p(prompt); return R(); }
 
-    public static I RI() { return IP(R()); }
-    public static D RD() { return DP(R()); }
-    public static T RT() { return TP(R()); }
+    public static I RI() => IP(R());
+    public static D RD() => DP(R());
+    public static T RT() => TP(R());
 
-    public static I RI(S prompt) { return IP(R(prompt)); }
-    public static D RD(S prompt) { return DP(R(prompt)); }
-    public static T RT(S prompt) { return TP(R(prompt)); }
+    public static I RI(S prompt) => IP(R(prompt));
+    public static D RD(S prompt) => DP(R(prompt));
+    public static T RT(S prompt) => TP(R(prompt));
 
     public static I RI(S prompt, int MaxTries) { while (MaxTries > 0) { try { return RI(prompt); } catch { MaxTries--; } } throw new Exception("Out of tries"); }
     public static D RD(S prompt, int MaxTries) { while (MaxTries > 0) { try { return RD(prompt); } catch { MaxTries--; } } throw new Exception("Out of tries"); }
     public static T RT(S prompt, int MaxTries) { while (MaxTries > 0) { try { return RT(prompt); } catch { MaxTries--; } } throw new Exception("Out of tries"); }
 
     //TODO: other Replace() overloads
-    public static S R(this S s, S oldValue, S newValue ) { return s.Replace(oldValue, newValue); }
-    
-    public static S Pad(this S s, I length) { if (length >= 0) return s.PadRight(length); else return s.PadLeft(length*-1);}
+    public static S R(this S s, S oldValue, S newValue ) =>  s.Replace(oldValue, newValue);
 
-    public static S S(this S s, I startIndex) { return s.Substring(startIndex); }
-    public static S S(this S s, I startIndex, I length) { return s.Substring(startIndex, length); }
+    public static S S(this S s, I startIndex) => s.Substring(startIndex);
+    public static S S(this S s, I startIndex, I length) => s.Substring(startIndex, length);
 
     //TODO: other IndexOf() overloads
-    public static I I(this S s, S value) { return s.IndexOf(value); }
-    public static I I(this S s, S value, I startIndex) { return s.IndexOf(value, startIndex); }
-    public static I I(this S s, S value, I startIndex, I count) { return s.IndexOf(value, startIndex, count); }
+    public static I I(this S s, S value) => s.IndexOf(value);
+    public static I I(this S s, S value, I startIndex) => s.IndexOf(value, startIndex);
+    public static I I(this S s, S value, I startIndex, I count) => s.IndexOf(value, startIndex, count);
 
     //TODO: CultureInfo overloads for ToUpper()/ToLower()
-    public static S l(this S s) { return s.ToLower(); }
-    public static S U(this S s) { return s.ToUpper(); }
+    public static S l(this S s) => s.ToLower();
+    public static S U(this S s) => s.ToUpper();
 
     //TODO: Trim() overloads
-    public static S T(this S s) { return s.Trim(); }
+    public static S T(this S s) => s.Trim();
 
     //TODO: convice C# language team to add extension property support
-    public static I L(this S s) { return s.Length; }
-    public static I IMx() { return Int32.MaxValue; }
-    public static I IMn() { return Int32.MinValue; }
-    public static D DMx() { return Double.MaxValue; }
-    public static D DMn() { return Double.MinValue; }
+    public static I L(this S s) => s.Length;
+    public static I IMx() =>  Int32.MaxValue;
+    public static I IMn() => Int32.MinValue;
+    public static D DMx() => D.MaxValue;
+    public static D DMn() => D.MinValue;
 
-    public static I L<T>(this IList<T> items) { return items.Count; }
-    public static I L<T>(this T[] items) { return items.Length; }
-    public static I L<T>(this IEnumerable<T> items) { return items.Count(); }
+    public static I L<T>(this IList<T> items) => items.Count;
+    public static I L<T>(this T[] items) => items.Length;
+    public static I L<T>(this IEnumerable<T> items) => items.Count();
 
     //TODO: Find a way to add a static method to a sealed type. May need to change the spec instead :(
-    public static I IP(S s) { return Int32.Parse(s); }
-    public static D DP(S s) { return Double.Parse(s); }
-    public static T TP(S s) { return DateTime.Parse(s); }
+    public static I IP(S s) => Int32.Parse(s);
+    public static D DP(S s) => D.Parse(s);
+    public static T TP(S s) => DateTime.Parse(s);
 }
